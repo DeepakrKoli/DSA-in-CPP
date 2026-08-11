7c2cd02eda4b811b114d88ebb65c3e7baf4da268
Deepak Koli <deepakrkoli.2003@gmail.com>
2026-08-11 12:56:49 +0530

    Learned Variables and DataTypes

---
+++ 2 - Variables and DataTypes/program1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // variable define 
+    int age = 22;
+    cout << "Your age is : " << age << endl;
+
+    // variable declaration
+    // int num;
+    // cout << num << endl;
+
+    // variable definition
+
+    int marks = 99;
+    cout << "Your marks : " << marks << "\n";
+
+    // variable manipulation / updation 
+    marks = 100;
+    cout << "Your new marks : " << marks;
+
+    
+    
+    return 0;
+    
+}
