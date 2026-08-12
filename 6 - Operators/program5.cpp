014fb8a00d42155d0663380357750e891c96d789
Deepak Koli <deepakrkoli.2003@gmail.com>
2026-08-12 10:34:36 +0530

    Learned Operators

---
+++ 6 - Operators/program5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    // Assignment Operator
+    
+    int a = 10;
+    a += 5; // 15
+    a -= 12; // 3
+    a *= 10; // 30
+    a /= 3; // 10
+    a %= 2; // 0
+
+    cout << a ;
+
+
+    
+    
+    return 0;
+    
+}
