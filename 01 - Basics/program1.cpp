c6ece63a1d86d70843faac8ee85cd21275de8428
Deepak Koli <deepakrkoli.2003@gmail.com>
2026-08-14 12:14:32 +0530

    Learned Basics

---
+++ 01 - Basics/program1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+
+    // First Program 
+    cout << "This is my first c++ program let's go ..." <<endl;
+
+
+    return 0;
+}
---
+++ 01 - Basics/program2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main() {
+
+    // Writing program without using namespace std;
+
+    std:: cout << "Deepak Koli" << std::endl;
+
+    return 0;
+
+
+}
